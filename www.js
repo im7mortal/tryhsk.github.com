e55b65dc4e3add10cc12a9d0d91bc5f66c2a9b8c
im7mortal <im7mortal@gmail.com>
2015-01-15 22:06:06 +0200

    2

--- www.js
+++ www.js
@@ -4,10 +4,16 @@ var express = require("express")
 	, fs = require('fs')
 	, app = express();
 var Facebook = require('facebook-node-sdk');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var port = Number(process.env.PORT || 5000);
 
 var index = fs.readFileSync('index.html');
 //app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({ secret: 'ewewq bar' }));
 app.use(logfmt.requestLogger());
 app.use(Facebook.middleware({ appId: '635617779878563', secret: '5d1fdfd275fef5abd6cbdfcb9c703616' }));
 
